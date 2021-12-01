6eedd70cc4cd590b307e70b79a496c56b9a8970c
JerzyDan <47829609+JerzyDan@users.noreply.github.com>
2021-12-01 18:51:26 +0100

    Api gateway (#4)
    * api-gateway service, eurekaClient added
    
    * Added gateway-service, minor changes in other services
    
    Co-authored-by: JerzyDan <jdan@10g.pl>

---
+++ traveler-notepad-backend/api-gateway/src/main/java/com/ironhack/apigateway/ApiGatewayConfiguration.java
@@ -0,0 +1,41 @@
+package com.ironhack.apigateway;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiGatewayConfiguration {
+
+    @Bean
+    public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
+        return builder.routes()
+
+                //Country Service
+                .route(p -> p.path("/api/v1/country/**")
+                        .uri("lb://COUNTRY-SERVICE"))
+                .route(p ->p.path("/api/v1/country**")
+                        .uri("lb://COUNTRY-SERVICE"))
+
+                //Place Service
+                .route(p -> p.path("/api/v1/places/**")
+                        .uri("lb://PLACE-SERVICE"))
+                .route(p ->p.path("/api/v1/places**")
+                        .uri("lb://PLACE-SERVICE"))
+
+                //Search Service
+                .route(p -> p.path("/api/v1/search/**")
+                        .uri("lb://SEARCH-SERVICE"))
+                .route(p ->p.path("/api/v1/search**")
+                        .uri("lb://SEARCH-SERVICE"))
+
+                //Report Service
+                .route(p -> p.path("/api/v1/report/**")
+                        .uri("lb://REPORT-SERVICE"))
+                .route(p ->p.path("/api/v1/report**")
+                        .uri("lb://REPORT-SERVICE"))
+
+                .build();
+    }
+}
--- traveler-notepad-backend/discovery-service/src/main/java/com/ironhack/discoveryservice/DiscoveryServiceApplication.java
+++ traveler-notepad-backend/discovery-service/src/main/java/com/ironhack/discoveryservice/DiscoveryServiceApplication.java
@@ -3,11 +3,9 @@ package com.ironhack.discoveryservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaServer
-@EnableFeignClients
 public class DiscoveryServiceApplication {
 
     public static void main(String[] args) {
