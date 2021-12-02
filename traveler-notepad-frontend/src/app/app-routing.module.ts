9a553678265d8422e6ffffa58f190785ae4b983b
JerzyDan <jdan@10g.pl>
2021-12-02 20:59:04 +0100

    Navbar, routing and components

--- traveler-notepad-frontend/src/app/app-routing.module.ts
+++ traveler-notepad-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
+import { SummaryComponent } from './summary/summary.component';
+import { SearchComponent } from './search/search.component';
+import { PlacesComponent } from './places/places.component';
+import { CountriesComponent } from './countries/countries.component';
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent
+  },
+  {
+    path: "countries",
+    component: CountriesComponent
+  },
+  {
+    path: "places",
+    component: PlacesComponent
+  },
+  {
+    path: "search",
+    component: SearchComponent
+  },
+  {
+    path: "summary",
+    component: SummaryComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
