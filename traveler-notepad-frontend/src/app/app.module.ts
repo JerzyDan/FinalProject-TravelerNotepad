b972c8dd203d92b29d71db9257a25a46f36365f6
JerzyDan <47829609+JerzyDan@users.noreply.github.com>
2021-12-03 20:05:54 +0100

    Navbar and footer (#5)
    * Navbar, routing and components
    
    * Added footer
    
    Co-authored-by: JerzyDan <jdan@10g.pl>

--- traveler-notepad-frontend/src/app/app.module.ts
+++ traveler-notepad-frontend/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { CountriesComponent } from './countries/countries.component';
+import { PlacesComponent } from './places/places.component';
+import { SearchComponent } from './search/search.component';
+import { SummaryComponent } from './summary/summary.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    CountriesComponent,
+    PlacesComponent,
+    SearchComponent,
+    SummaryComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
