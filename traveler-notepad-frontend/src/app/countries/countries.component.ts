c3a0626d275d1a81553dd4a78cab5792cae5a1db
JerzyDan <jdan@10g.pl>
2021-12-04 12:51:39 +0100

    Added countries service and countries model

--- traveler-notepad-frontend/src/app/countries/countries.component.ts
+++ traveler-notepad-frontend/src/app/countries/countries.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CountriesComponent implements OnInit {
 
+ 
+
   constructor() { }
 
   ngOnInit(): void {
---
+++ traveler-notepad-frontend/src/app/models/countries.model.ts
@@ -0,0 +1,7 @@
+export interface Country {
+    id: number,
+    name: string,
+    officialLanguage: string,
+    climate: string,
+    area: string
+}
