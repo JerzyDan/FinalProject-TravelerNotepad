728cbf1d06b4de62d057e2ecfb97f165c0d4ac5a
JerzyDan <jdan@10g.pl>
2021-12-13 21:42:25 +0100

    Search service, country FE upgrade

---
+++ traveler-notepad-frontend/src/app/search.service.ts
@@ -0,0 +1,26 @@
+import { Place } from './models/places.model';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SearchService {
+
+  private readonly baseUrl = 'http://localhost:8300/api/v1/search';
+
+  constructor(private http: HttpClient) { }
+
+  getPlaceByName(name: string) : Observable<Place> {
+    return this.http.get<Place>(`${this.baseUrl}/${name}`);
+  }
+
+  getPlacesByCity(city: string) : Observable<Place[]> {
+    return this.http.get<Place[]>(`${this.baseUrl}/c/${city}`);
+  }
+
+  getPlacesByStatus(status: string) : Observable<Place[]> {
+    return this.http.get<Place[]>(`${this.baseUrl}/s/${status}`);
+  }
+}
